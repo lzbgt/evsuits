7a8a0adddde0f14c6ca553798abb4c98532f5162
blu <lzbgt@126.com>
2019-09-23 15:19:34 +0800

    big refacting of communitation architect

--- opencv-motion-detect/dirmon.cpp
+++ opencv-motion-detect/dirmon.cpp
@@ -14,7 +14,7 @@ int CreateDirMon(monitor **m, string path, string ext, vector<string> &&events, 
 {
     vector<string> paths= {path};
     vector<fsw_event_type_filter> event_filters = {{fsw_event_flag::Created}};
-    vector<monitor_filter> flt = {{.text=".*\.mp4", .type=fsw_filter_type::filter_include, .case_sensitive = false, .extended=false}};
+    vector<monitor_filter> flt = {{.text=".*\\.mp4", .type=fsw_filter_type::filter_include, .case_sensitive = false, .extended=false}};
     *m = monitor_factory::create_monitor(
              fsw_monitor_type::system_default_monitor_type,
              paths,
