704a6290d74f4435d0dd621c2ab8700f7455516d
blu <lzbgt@126.com>
2019-10-09 16:09:37 +0800

    upload video files

--- opencv-motion-detect/test_regex.cpp
+++ opencv-motion-detect/test_regex.cpp
@@ -19,9 +19,9 @@ json getModulesOperFromConfDiff(json& oldConfig, json &newConfig, json &diff, st
     ret["msg"] = "ok";
     ret["data"] = json();
     bool hasError = false;
-    spdlog::info("matching {}", diff.dump());
+    spdlog::info("matching {}, size:{}, type:{}", diff.dump(), diff.size(), diff.type_name());
     try{
-        for(auto &d: diff.array()) {
+        for(auto &d: diff) {
             spdlog::info("d :{}, {}", d.dump(), d.size());
             if(d.count("path") != 0) {
                 string path_ = d["path"];
@@ -35,10 +35,8 @@ json getModulesOperFromConfDiff(json& oldConfig, json &newConfig, json &diff, st
                 string ipcRegStr = string("/") + sn + "/ipcs/(\\d+)/(\\w+)";
                 std::smatch results;
                 std::regex ipcRegex(ipcRegStr);
-                spdlog::info("x diff: {}", diff.dump());
                 if (std::regex_match(path_, results, ipcRegex)) {
                     if (results.size() == 3) {
-                        spdlog::info("matched ipc: {}",path_);
                         matched = true;
                         int ipcIdx = stoi(results[1].str());
                         string tag = results[2].str();
@@ -76,14 +74,11 @@ json getModulesOperFromConfDiff(json& oldConfig, json &newConfig, json &diff, st
                                 }
                             }
                         }
-                    }else{
-                        for(auto &i: results) {
-                            spdlog::info("\t{}", i.str());
-                        }
                     }
-                }else{
-                    spdlog::info("no match for ipc", path_);
                 }
+                // else{
+                //     spdlog::info("no match for ipc", path_);
+                // }
 
                 // match module config
                 if(!matched && !hasError) {
@@ -93,14 +88,13 @@ json getModulesOperFromConfDiff(json& oldConfig, json &newConfig, json &diff, st
                     std::smatch results2;
                     if (std::regex_match(path_, results2, moduleRegex)) {
                         if (results2.size() == 5) {
-                            spdlog::info("matched module: {}",path_);
                             int ipcIdx = stoi(results2[1].str());
-                            int modIdx = stoi(results[3].str());
-                            string modName = results[2].str();
-                            string propName = results[4].str();
+                            int modIdx = stoi(results2[3].str());
+                            string modName = results2[2].str();
+                            string propName = results2[4].str();
                             if(d["op"] == "replace"||d["op"] == "add") {   
-                                auto &oldMod = oldConfig[sn]["ipcs"][ipcIdx]["modules"][modIdx];
-                                auto &newMod = newConfig[sn]["ipcs"][ipcIdx]["modules"][modIdx];
+                                auto &oldMod = oldConfig[sn]["ipcs"][ipcIdx]["modules"][modName][modIdx];
+                                auto &newMod = newConfig[sn]["ipcs"][ipcIdx]["modules"][modName][modIdx];
                                 if(oldMod.count("iid") == 0 || newMod.count("iid") == 0) {
                                     string msg = fmt::format("invalid module config ipcs[{}]['modules'][{}][{}] having no iid field", ipcIdx, modName, modIdx);
                                     spdlog::error(msg);
@@ -119,8 +113,8 @@ json getModulesOperFromConfDiff(json& oldConfig, json &newConfig, json &diff, st
                                         }
                                     }
 
-                                    string oldGid = sn + ":" + modName + ":" + oldMod["iid"].get<string>();
-                                    string newGid = sn + ":" + modName + ":" + newMod["iid"].get<string>();
+                                    string oldGid = sn + ":" + modName + ":" + to_string(oldMod["iid"].get<int>());
+                                    string newGid = sn + ":" + modName + ":" + to_string(newMod["iid"].get<int>());
 
                                     if(oldGid != newGid) {
                                         ret["data"][oldGid] = 0;
@@ -130,10 +124,10 @@ json getModulesOperFromConfDiff(json& oldConfig, json &newConfig, json &diff, st
                                         if(newMod["enabled"].get<int>() == 0) {
                                             ret["data"][newGid] = 0;
                                         }else{
-                                            ret["data"][newGid] = 2;
+                                            ret["data"][newGid] = 1;
                                         }
-                                    }else{
-                                        // disabled
+                                    }else{ // other prop modification
+                                        // it was disabled. just ignore
                                         if(ret["data"].count(newGid) != 0 && ret["data"][newGid].get<int>() == 0) {
                                             // nop
                                         }else{
@@ -143,14 +137,11 @@ json getModulesOperFromConfDiff(json& oldConfig, json &newConfig, json &diff, st
                                     }
                                 }
                             }
-                        }else{
-                            for(auto &i: results) {
-                                spdlog::info("\t{}", i.str());
-                            }
                         }
-                    }else{
-                        spdlog::info("no match for module {}", path_);
                     }
+                    // else{
+                    //     spdlog::info("no match for module {}", path_);
+                    // }
                 } 
 
                 if(hasError){
@@ -176,7 +167,7 @@ int main(){
     config = config["data"];
     iff2 >> config2;
     config2 = config2["data"];
-    json dif = json::diff(config2, config).dump();
+    json dif = json::diff(config2, config);
     spdlog::info("diff: {}", dif.dump());
     auto ret = getModulesOperFromConfDiff(config2, config, dif, "PSBV7GKN");
     spdlog::info("parse: {}", ret.dump());
