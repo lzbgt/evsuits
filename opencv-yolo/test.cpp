d5a85e44f281cf5baf0b0ec935f0f45a5c20bf8a
blu <lzbgt@126.com>
2020-01-02 15:20:13 +0800

    video ai

--- opencv-yolo/test.cpp
+++ opencv-yolo/test.cpp
@@ -1,16 +1,7 @@
-#include <fstream>
-#include <sstream>
-#include <iostream>
 
-#ifdef _MY_HEADERS_
-#include <opencv2/core/types_c.h>
-#include <opencv2/dnn.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
-#else
 #include <opencv2/core/types_c.h>
 #include <opencv2/opencv.hpp>
-#endif
+#include <iostream>
 
 using namespace std;
 using namespace cv;
@@ -18,5 +9,5 @@ using namespace cv;
 int main(){
   VideoCapture cap;
   if(!cap.open("a.mp4"))
-  cout << "failed to open";
+  cout << "failed to open\n";
 }
